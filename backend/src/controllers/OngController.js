376caad207194349e8df2630621aa1eaebfea184
Julio Rodrigues <julio_cesar.jcr@hotmail.com>
2020-03-26 15:35:26 -0300

    Back-end da aplicação

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,29 @@
+const connection = require('../database/connection');
+const crypto = require('crypto');
+
+
+module.exports = {
+
+    async index (request, response){
+        const ongs = await connection('ongs').select('*'); //listar as ongs cadstradas 
+    
+    return response.json(ongs);
+    },
+    
+    async create(request, response){
+        const {name, email, whatsapp, city, uf} = request.body;
+
+        const id = crypto.randomBytes(4).toString('HEX'); //criar id para as ongs cadastradas
+    
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+        })
+
+        return response.json({  id  });
+    }
+};
